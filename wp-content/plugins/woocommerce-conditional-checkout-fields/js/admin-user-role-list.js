52a25ecfb9033a8fb363849eead2b99b0dce71c0
loftwah <dean@deanlofts.xyz>
2021-05-09 17:56:12 +0800

    deleted plugin `woocommerce-conditional-checkout-fields.php`

--- wp-content/plugins/woocommerce-conditional-checkout-fields/js/admin-user-role-list.js
+++
@@ -1,8 +0,0 @@
-function wcccf_init_user_role_select2()
-{
-	jQuery(".js-data-user-role").select2(
-		{
-			width:300,
-			 minimumInputLength: 0
-		});
-}
--- wp-content/plugins/woocommerce-conditional-checkout-fields/templates/admin_conditional_group_item.php
+++
@@ -1,3 +0,0 @@
-<div class="conditional_row">
-	<?php include WCCCF_PLUGIN_ABS_PATH.'/templates/admin_conditional_item.php'; ?>
-</div>
