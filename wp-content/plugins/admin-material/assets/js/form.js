b77e19df73099c92416c6186df4db998d148596e
loftwah <dean@deanlofts.xyz>
2021-05-09 17:56:09 +0800

    deleted plugin `theme.scss`

--- wp-content/plugins/admin-material/assets/js/form.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  "use strict";
-  $(document).on('blur', 'input, textarea', function(e){
-    $(this).val() ? $(this).addClass('has-value') : $(this).removeClass('has-value');
-  });
-  jQuery(document).ready(function(){
-	jQuery("input").trigger('blur');
-  });
-})(jQuery);
--- wp-content/plugins/admin-material/scss/scss.php
+++
@@ -1,5 +0,0 @@
-<?php
-$directory = dirname ( __FILE__ )."/scss";
-
-require "scss.inc.php";
-scss_server::serveFrom($directory);
