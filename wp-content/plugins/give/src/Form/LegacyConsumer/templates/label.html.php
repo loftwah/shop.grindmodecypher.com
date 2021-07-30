072286e79e8a7688c648c954f1e411fff7ac184d
loftwah <dean@deanlofts.xyz>
2021-07-30 21:46:28 +0800

    deleted plugin `wpml-config.xml`

--- wp-content/plugins/give/src/Form/LegacyConsumer/templates/label.html.php
+++
@@ -1,6 +0,0 @@
-<label class="give-label" for="give-<?php echo $field->getName(); ?>">
-	<?php echo $field->getLabel(); ?>
-	<?php if ( $field->isRequired() ) : ?>
-	&nbsp;<span class="give-required-indicator">*</span>
-	<?php endif; ?>
-</label>
