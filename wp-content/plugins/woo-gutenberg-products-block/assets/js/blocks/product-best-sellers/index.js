821a4c9c824abcef9e7647c2859036c3b89cf583
loftwah <dean@deanlofts.xyz>
2021-12-17 23:59:27 +0800

    installed plugin `WooCommerce Blocks` version 6.5.0

---
+++ wp-content/plugins/woo-gutenberg-products-block/assets/js/blocks/product-best-sellers/index.js
@@ -0,0 +1,71 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { without } from 'lodash';
+import { Icon, stonks } from '@woocommerce/icons';
+import { createBlock, registerBlockType } from '@wordpress/blocks';
+
+/**
+ * Internal dependencies
+ */
+import Block from './block';
+import sharedAttributes, {
+	sharedAttributeBlockTypes,
+} from '../../utils/shared-attributes';
+
+registerBlockType( 'woocommerce/product-best-sellers', {
+	title: __( 'Best Selling Products', 'woo-gutenberg-products-block' ),
+	icon: {
+		src: <Icon srcElement={ stonks } />,
+		foreground: '#7f54b3',
+	},
+	category: 'woocommerce',
+	keywords: [ __( 'WooCommerce', 'woo-gutenberg-products-block' ) ],
+	description: __(
+		'Display a grid of your all-time best selling products.',
+		'woo-gutenberg-products-block'
+	),
+	supports: {
+		align: [ 'wide', 'full' ],
+		html: false,
+	},
+	example: {
+		attributes: {
+			isPreview: true,
+		},
+	},
+	attributes: {
+		...sharedAttributes,
+	},
+
+	transforms: {
+		from: [
+			{
+				type: 'block',
+				blocks: without(
+					sharedAttributeBlockTypes,
+					'woocommerce/product-best-sellers'
+				),
+				transform: ( attributes ) =>
+					createBlock(
+						'woocommerce/product-best-sellers',
+						attributes
+					),
+			},
+		],
+	},
+
+	/**
+	 * Renders and manages the block.
+	 *
+	 * @param {Object} props Props to pass to block.
+	 */
+	edit( props ) {
+		return <Block { ...props } />;
+	},
+
+	save() {
+		return null;
+	},
+} );
