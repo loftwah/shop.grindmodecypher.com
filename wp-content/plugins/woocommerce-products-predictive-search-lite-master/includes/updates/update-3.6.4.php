3c633f81993cf31984e3a1589e309855b6c72b5e
loftwah <dean@deanlofts.xyz>
2021-12-18 00:27:26 +0800

    deleted plugin `Predictive Search for WooCommerce` version 5.3.16

--- wp-content/plugins/woocommerce-products-predictive-search-lite-master/includes/updates/update-3.6.4.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
-global $wpdb;
-$sql = "ALTER TABLE ". $wpdb->prefix . "ps_product_sku ADD post_parent BIGINT NOT NULL DEFAULT 0";
-$wpdb->query($sql);
