5c8f37ececf3eef1579ac8a7f806b72a9dc69138
loftwah <dean@deanlofts.xyz>
2022-02-28 21:17:32 +0800

    updated plugin `WooCommerce Stripe Gateway` version 6.2.0

--- wp-content/plugins/woocommerce-gateway-stripe/build/payment_gateways.asset.php
+++ wp-content/plugins/woocommerce-gateway-stripe/build/payment_gateways.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wc-navigation', 'wp-components', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '8cec8ac706a3066dab1bf95c3ea3f8f7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wc-navigation', 'wp-components', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'b4b8e86ff3d285f29625e368e9c51bbf');
